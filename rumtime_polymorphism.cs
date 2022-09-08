0d85bf4f369a2f92c3d9291a99a10bae7a9e78d8
shaswatmisra2310 <43674831+shaswatmisra2310@users.noreply.github.com>
2022-09-08 11:43:21 +0530

    Create rumtime_polymorphism.cs

---
+++ rumtime_polymorphism.cs
@@ -0,0 +1,46 @@
+namespace Myproject
+{
+
+    class FinancialCalc
+    {
+        int principle;
+        int interest_rate;
+        int years;
+        
+        public void FinancialCalc(int p,int r, inr y)
+        {
+        this.principle=p;
+        this.interest_rate=r;
+        this.years=y;
+        }
+        
+        public virtual calcinterest()
+        {
+        }
+    }
+    
+    class SimpleInterest:FinancialCalc
+    {
+    public override calcinterest()
+    {
+     Console.WriteLine(principle*interest_rate*years);
+    }
+    }
+    class CompoundInterest:FinancialCalc
+    {
+    public override calcinterest()
+    {
+     Console.WriteLine(principle*((1+interest_rate)^years-1));
+    }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        FinancialCalc newUserFinance = new FinanceCalc[2];
+        newUserFinance[0]=new SimpleInterest(20,0.01,2);
+        
+        }
+    }
+
+}

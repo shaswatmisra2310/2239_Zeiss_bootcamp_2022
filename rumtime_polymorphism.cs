72f68c879fd4f55bc3930309d47cea071a88a500
shaswatmisra2310 <43674831+shaswatmisra2310@users.noreply.github.com>
2022-09-08 11:51:33 +0530

    Update rumtime_polymorphism.cs

--- rumtime_polymorphism.cs
+++ rumtime_polymorphism.cs
@@ -1,48 +1,102 @@
-namespace Myproject
+namespace ConsoleApp16
 {
 
-    class FinancialCalc
+    class TataCar
     {
-        int principle;
-        int interest_rate;
-        int years;
-        
-        public void FinancialCalc(int p,int r, int y)
-        {
-        this.principle=p;
-        this.interest_rate=r;
-        this.years=y;
+        //Dependency
+        RevtronEngine _engine=new RevtronEngine(); //composition
+        public void Ignite() {
+        _engine.Start();
         }
-        
-        public virtual calcinterest()
-        {
+        public void Halt() {
+            _engine.Stop();
         }
     }
-    
-    class SimpleInterest:FinancialCalc
+    class RevtronEngine
     {
-    public override calcinterest()
-    {
-     Console.WriteLine(principle*interest_rate*years);
+        public void Start() { }
+        public void Stop() { }
     }
-    }
-    class CompoundInterest:FinancialCalc
+    class VericoreEngine
     {
-    public override calcinterest()
-    {
-     Console.WriteLine(principle*((1+interest_rate)^years-1));
+
     }
+    class KrytoechEngine
+    {
+
     }
     internal class Program
     {
         static void Main(string[] args)
         {
-        FinancialCalc newUserFinance = new FinanceCalc[2];
-        newUserFinance[0]=new SimpleInterest(20,0.01,2);
-        newUserFinance[1]=new ComoundInterest(20,0.01,4);
-        foreach(FinanceCalc f in newUserFinance)
-            f.calcinterest();
+            TataCar _nexon = new TataCar();
+
+            TataCar _harrier = new TataCar(); //Kryotech engine
+
+            TataCar _hexa = new TataCar();//vericore engine
+
+            FamilyOccassion _family = new FamilyOccassion();
+            Parent _parentInstance = new Parent();
+            _family.Visit(_parentInstance);
+
+            _parentInstance = new Child_One(); //Runtime Polymorphism
+            _family.Visit(_parentInstance);
+
+            _parentInstance = new Child_Two();
+            _family.Visit(_parentInstance);
+
+            _parentInstance = new Child_Three();
+            _family.Visit(_parentInstance);
+
         }
     }
+
+
+    public class FamilyOccassion
+    {
+
+        public void Visit(Parent  familyMember)
+        {
+            familyMember.Participate(); //Runtime Polymorphism
+            
+        }
+        
+    }
+
+    public class Parent {
+    
+        public virtual void Participate()
+        {
+            Console.WriteLine("Parent ...Participate ");
+        }
+    }
+    public class Child_One:Parent {
+
+        public override void Participate()
+        {
+            Console.WriteLine("ChildOne...Participate");
+        }
+        public void Smoke()
+        {
+
+        }
+    }
+    public class Child_Two:Parent {
+
+        public override void Participate()
+        {
+            Console.WriteLine("ChildTwo...Participate");
+        }
+
+    }
+    public class Child_Three : Parent {
+        public override void Participate()
+        {
+            Console.WriteLine("ChildThree...Participate");
+        }
+
+    }
+
+
 
 }
